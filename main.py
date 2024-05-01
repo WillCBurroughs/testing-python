41d58b48dd970a07754274ab1dbd432894b7a40d
WillCBurroughs <willcburroughs@gmail.com>
2024-05-01 18:38:15 -0400

    Adding simple print and string functionality for reference

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 name = input("What's your name? ")
+
+name = name.strip()
+
+name = name.title()
+
 print("hello ", name, end="\n", sep="")
+
+print("\"So how do you add a quote?\" - Asked " + name)
+
+print("\"How can I put variables in the middle of a string?\"")
+
+print(f"Good question, {name}")
