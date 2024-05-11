7e218c9f86acba4ed3fb0dc2a52c9f440446afb0
WillCBurroughs <willcburroughs@gmail.com>
2024-05-11 12:09:33 -0500

    Adding sys.exit

--- lesson_4/average.py
+++ lesson_4/average.py
@@ -1,3 +1,9 @@
 import sys
 
-print("Hello, my name is", sys.argv[1])
+# Error checking 
+if len(sys.argv) < 2:
+    sys.exit("Too Few Arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many argumenets")
+
+print("Hello my name is", sys.argv[1])
