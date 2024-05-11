a7e20bc6a0a789b5fbae10011650645f4622836c
WillCBurroughs <willcburroughs@gmail.com>
2024-05-11 12:15:34 -0500

    Adding slice

--- lesson_4/average.py
+++ lesson_4/average.py
@@ -3,7 +3,8 @@ import sys
 # Error checking 
 if len(sys.argv) < 2:
     sys.exit("Too Few Arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many argumenets")
 
 print("Hello my name is", sys.argv[1])
+
+for arg in sys.argv[1:]:
+    print(arg)
