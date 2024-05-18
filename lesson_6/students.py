9af0fd02b5f762ddaaad091805f88d97ad34f527
WillCBurroughs <willcburroughs@gmail.com>
2024-05-18 16:42:46 -0400

    lesson_6

--- lesson_6/students.py
+++ lesson_6/students.py
@@ -1,4 +1,19 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is a student at {row[1]}")
+        name, house = line.rstrip().split(",")
+
+        # Making dict of student to place
+        student = {"name": name, "house": house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key = get_house, reverse=True):
+    print(f"{student['name']} is in house {student['house']}")
+
