cf48b53a86b60885d7326a3dbc866bdbaf0ac28d
WillCBurroughs <willcburroughs@gmail.com>
2024-05-21 20:49:30 -0400

    Adding url replace

--- lesson_7/format.py
+++ lesson_7/format.py
@@ -2,7 +2,7 @@ import re
 
 name = input("What's your name? ").strip()
 
-if "," in name:
-    last, first = name.split(", ")
-    name = f"{first} {last}"
-print(f"hello {name}")
+# Adding walrus operator (:=) to assign value and ask boolean question 
+if matches := re.search(r"^(.+), *(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
+print(f"hello, {name}")
