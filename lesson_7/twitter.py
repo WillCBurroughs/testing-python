9b7631322672e91c4821296a89ec99fe364442bf
WillCBurroughs <willcburroughs@gmail.com>
2024-05-22 20:11:45 -0400

    Adding re.sub example

--- lesson_7/twitter.py
+++ lesson_7/twitter.py
@@ -1,5 +1,7 @@
+import re
+
 url = input("URL: ").strip()
 
-username = url.replace("https://twitter.com/", "")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 
 print(f"Username: {username}")
