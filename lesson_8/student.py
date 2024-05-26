bf300ffba5b31597cf3c23312e239fda5cd66f07
WillCBurroughs <willcburroughs@gmail.com>
2024-05-26 14:16:15 -0500

    Adding patronus

--- lesson_8/student.py
+++ lesson_8/student.py
@@ -1,18 +1,25 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name: 
             raise ValueError("Missing Name")
         self.name = name 
+        if house not in ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]:
+            raise ValueError("Invalid House")
         self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name,house)
+    patronus = input("Patronus: ")
+    return Student(name,house,patronus)
     
 
 if __name__ == "__main__":
