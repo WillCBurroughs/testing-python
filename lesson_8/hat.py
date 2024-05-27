bee6effe7bcd1df274e7d1fd68bc9527dafc5dcd
WillCBurroughs <willcburroughs@gmail.com>
2024-05-27 06:57:44 -0500

    Adding classmethod

--- lesson_8/hat.py
+++ lesson_8/hat.py
@@ -2,12 +2,11 @@ import random
 
 class Hat:
 
-    def __init__(self):
-        self.houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
 
-    def sort(self, name): 
-        print(name, "is in", random.choice(self.houses))
+    @classmethod
+    def sort(cls, name): 
+        print(name, "is in", random.choice(cls.houses))
 
 
-hat = Hat()
-hat.sort("Harry")
+Hat.sort("Harry")
