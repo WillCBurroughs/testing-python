5de302ddf443be7f04c998acaea7ad04d4d7e070
WillCBurroughs <willcburroughs@gmail.com>
2024-06-06 16:40:07 -0400

    adding new folder for python for everyone lessons

---
+++ python_for_everyone/chapter_2.py
@@ -0,0 +1,6 @@
+# Can not use reserved word as variable names 
+# variable names must start with letter or underscore 
+
+# can have letters and numbers and underscores 
+# bad practice to use same words for variables only differentiating based on case 
+
