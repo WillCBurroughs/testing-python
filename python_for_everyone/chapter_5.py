20f42e4588e049f1601fd3802e1e74c34f331902
WillCBurroughs <willcburroughs@gmail.com>
2024-06-22 13:41:11 -0400

    Adding simple program for averages

---
+++ python_for_everyone/chapter_5.py
@@ -0,0 +1,25 @@
+def main():
+
+    user_sum = 0 
+    user_total = 0
+    user_average = 0 
+
+    test_input = ""
+
+    while test_input != "done":
+        test_input = input("What value would you like to add? ")
+
+        if test_input == "done":
+            user_average = user_sum / user_total
+            print(user_sum, user_total, user_average)
+            break
+
+        test_input = int(test_input)
+
+        user_sum += test_input
+        user_total += 1
+
+
+
+if __name__ == "__main__":
+    main()
