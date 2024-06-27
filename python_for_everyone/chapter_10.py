df62b0cb945d1cfe11932a3647424c11f23dfa3a
WillCBurroughs <willcburroughs@gmail.com>
2024-06-27 16:39:09 -0400

    Adding left sided tuples

--- python_for_everyone/chapter_10.py
+++ python_for_everyone/chapter_10.py
@@ -2,6 +2,11 @@ def main():
     # Tuples are immutable
     simple_tuple = ('hello', 'hey', 'oye')
     print(simple_tuple)
+    print(dir(simple_tuple))
+
+    # left sided tuples 
+    (hello, ayy) = ("hello", "ayy")
+    print(hello)
 
 if __name__ == "__main__":
     main()
