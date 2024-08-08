2e5d5510312e74ca35858946fab4b562542aa1e8
WillCBurroughs <willcburroughs@gmail.com>
2024-08-08 16:13:26 -0400

    Feature: Start of db

--- intro_comp_sci/new_to_do.py
+++ intro_comp_sci/new_to_do.py
@@ -1,5 +1,21 @@
-def main():
-    print("This will be new to do")
+import psycopg2
 
-if __name__ == "__main__":
-    main()
+conn = psycopg2.connect(host='localhost', dbname="postgres", user="postgres")
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS person(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    gender CHAR
+);
+""")
+
+conn.commit()
+
+cur.close()
+conn.close()
+
+# Setup database, Add UI 
